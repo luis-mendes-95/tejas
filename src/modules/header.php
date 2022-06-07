ab37e291b2e33725cfc3dd1298e34750383d45a3
luis-mendes-95 <luis.mendes.95@hotmail.com>
2022-06-07 19:04:14 -0300

    adding external php file as header

---
+++ src/modules/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+<link rel="stylesheet" href="./src/css/headerstyle.css">
+</head>
+<body>
+<header class="menu">
+        <div class="container">
+            <div class="menu-flex">
+                <div class="menu-column">
+                    <a href="/index.html">
+                        <img src="/src/img/tejaslogowhite.png" alt="logo" class="logo">
+                    </a>
+                    <nav class="menu-nav">
+                        <ul>
+                            <li><a href="/store.html">CAMISETAS</li></a>
+                            <li><a href="/store.html">GRÁFICA</li></a>
+                            <li><a href="/store.html">COMUNICAÇÃO VISUAL</li></a>
+                            <li><a href="/store.html">IDENTIDADE DIGITAL</li></a>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="menu-column menu-second-column">
+                    <a href="" class="menu-link-button">COMBOS</a>
+                </div>
+            </div>
+        </div>
+    </header>    
+</body>
+</html>
