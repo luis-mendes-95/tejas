9ad91770dc1e33e2d9b6ad6cca25266fb8b7567d
luis-mendes-95 <luis.mendes.95@hotmail.com>
2022-07-25 16:26:31 -0300

    alimentando estruturas de ordens de servico

---
+++ src/js/malhas.js
@@ -0,0 +1,10 @@
+const malhas = [
+    {
+        id: 1,
+        nome: "Malha Extreme Combat 92% Poliéster 8% Elastano Proteção UV",
+        largura: 1.58,
+        comprimentoKilo: 3.61,
+        precoMetro: 15.75,
+    }
+
+]
---
+++ src/js/orders.js
@@ -0,0 +1,91 @@
+let orders = [
+    {
+        id : 1,
+        client : "Ariane da Luz Poluceno",
+        sold : [
+            {
+                itemName : "WINDBANNER 2,20M ALTURA COMPLETO",
+                itemPrice : 169.00,
+                costing: [
+                    {
+                        itemName: "Metro Linear Tactel (Larg: 158cm)",
+                        itemPrice: 14.5,
+                        qty: 1.5,
+                        totalCosting: 14.5 * 1.5
+                    },
+                    {
+                        itemName: "Haste de Alumínio",
+                        itemPrice: 25.00,
+                        qty: 1,
+                        totalCosting: 25.00 * 1
+                    },
+                    {
+                        itemName: "Base de Concreto",
+                        itemPrice: 25.00,
+                        qty: 1,
+                        totalCosting: 25.00 * 1
+                    },
+                    {
+                        itemName: "Costura Windbanner",
+                        itemPrice: 10.00,
+                        qty: 1,
+                        totalCosting: 10.00 * 1
+                    },
+                ],
+                qty : 2,
+                totalDisccount : 119.00
+            },
+            {
+                itemName : "BANDEIRA 1M X 1M",
+                itemPrice : 75.00,
+                costing: [
+                    {
+                        itemName: "Metro Linear Tactel (Larg: 158cm)",
+                        itemPrice: 14.5,
+                        qty: 1,
+                        totalCosting: 14.5 * 1
+                    },
+                    {
+                        itemName: "Costura Bandeira Quadrada",
+                        itemPrice: 10.00,
+                        qty: 1,
+                        totalCosting: 10.00 * 1
+                    },
+                ],
+                qty : 2,
+                totalDisccount : 100.00
+            },
+            {
+                itemName : "CAPA DE ALMOFADA 40CM X 40CM",
+                itemPrice : 29.00,
+                costing: [
+                    {
+                        itemName: "Metro Linear Tactel (Larg: 158cm)",
+                        itemPrice: 14.5,
+                        qty: 0.5,
+                        totalCosting: 14.5 * 0.5
+                        provider
+                    },
+                    {
+                        itemName: "Costura Capa Almofada",
+                        itemPrice: 5.00,
+                        qty: 1,
+                        totalCosting: 5.00 * 1
+                    },
+                    {
+                        itemName: "Zíper Invisível",
+                        itemPrice: 2.00,
+                        qty: 1,
+                        totalCosting: 2.00 * 1
+                    },
+                ],
+                qty : 2,
+                totalDisccount : 44.00
+            },
+            ],
+
+
+
+
+    },
+]
